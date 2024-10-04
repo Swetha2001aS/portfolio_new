9c876c4f19544c192cd92a8782b5a3832ef935ed
swethaas <swetha2001as@gmail.com>
2024-10-04 20:45:35 +0530

    modify template and host

--- light/php/send-email.php
+++ light/php/send-email.php
@@ -1,6 +1,5 @@
 <?php
 
-// Replace this with your own email address
 $to = 'swetha2001as@gmail.com';
 
 function url(){
@@ -12,18 +11,16 @@ function url(){
 }
 
 if($_POST) {
-
    $name = trim(stripslashes($_POST['name']));
    $email = trim(stripslashes($_POST['email']));
    $subject = trim(stripslashes($_POST['subject']));
    $contact_message = trim(stripslashes($_POST['message']));
-
    
-	if ($subject == '') { $subject = "Contact Form Submission"; }
+   if ($subject == '') { $subject = "Contact Form Submission"; }
 
    // Set Message
-   $message .= "Email from: " . $name . "<br />";
-	 $message .= "Email address: " . $email . "<br />";
+   $message  = "Email from: " . $name . "<br />";
+   $message .= "Email address: " . $email . "<br />";
    $message .= "Message: <br />";
    $message .= nl2br($contact_message);
    $message .= "<br /> ----- <br /> This email was sent from your site " . url() . " contact form. <br />";
@@ -32,17 +29,19 @@ if($_POST) {
    $from =  $name . " <" . $email . ">";
 
    // Email Headers
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $email . "\r\n";
- 	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+   $headers = "From: " . $from . "\r\n";
+   $headers .= "Reply-To: " . $email . "\r\n";
+   $headers .= "MIME-Version: 1.0\r\n";
+   $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-   ini_set("sendmail_from", $to); // for windows server
+   ini_set("sendmail_from", $to); // for Windows server
    $mail = mail($to, $subject, $message, $headers);
 
-	if ($mail) { echo "OK"; }
-   else { echo "Something went wrong. Please try again."; }
-
+   if ($mail) { 
+       echo "OK"; 
+   } else { 
+       error_log("Mail error: " . error_get_last()['message']); // Log the error
+       echo "Something went wrong. Please try again."; 
+   }
 }
-
 ?>
